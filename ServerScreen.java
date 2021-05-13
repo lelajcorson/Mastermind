b34b3bb50d1b9f0ded89b8b986f10e822ee96c5e
lelajcorson <lelajcorson@gmail.com>
2021-05-12 19:30:52 -0700

    cleaned up code

--- ServerScreen.java
+++ ServerScreen.java
@@ -66,31 +66,6 @@ public class ServerScreen extends JPanel implements ActionListener{
 		submit.setBounds(350, 500, 100, 40);
 		submit.addActionListener(this);
 		add(submit);
-
-        // messages = "";
-        // toSend = false;
-
-        // textfield = new JTextField(20);
-        // textfield.setBounds(280,110,200,30);
-        // this.add(textfield);
-        // Action action = new AbstractAction(){
-        //   @Override
-        //   public void actionPerformed(ActionEvent e)
-        //   {
-        //     sendText = textfield.getText();
-        //     textfield.setText("");
-        //     toSend = true;
-        //     System.out.println("toSend is true");
-        //   }
-        // };
-        // textfield.addActionListener(action);
-
-        // textArea = new JTextArea(messages);
-        // scrollPane = new JScrollPane(textArea);
-        // scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        // scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        // scrollPane.setBounds(20,50,220,530);
-        // this.add(scrollPane);
     }
 
     public Dimension getPreferredSize() {
@@ -223,30 +198,6 @@ public class ServerScreen extends JPanel implements ActionListener{
         catch(ClassNotFoundException e){
             System.out.println(e);
         }
-
-
-        //     //System.out.println("in while true");
-        //     BufferedReader in = new BufferedReader(new InputStreamReader(cSocket.getInputStream()));
-        //     PushbackInputStream pin = new PushbackInputStream(cSocket.getInputStream());
-        //     PrintWriter out = new PrintWriter(cSocket.getOutputStream(), true);
-        //     if(pin.available() != 0){
-        //       String newMessage = in.readLine();
-        //       messages += "Client:" + newMessage + "\n";
-        //       textArea.setText(messages);
-
-        //       //in.close();
-
-        //       repaint();
-        //     }
-        //     else if(toSend){
-        //       messages += "Server: " + sendText + "\n";
-        //       textArea.setText(messages);
-
-        //       out.println(sendText);
-        //       //out.close();
-
-        //       toSend = false;
-        //     }
     }
 
     public void actionPerformed(ActionEvent e){
